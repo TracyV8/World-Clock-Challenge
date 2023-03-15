96bcf113aede0d2182bcc9ad6645de3e41d89f4d
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-03-15 14:26:56 +0800

    added new tiem zone data in js

--- css/index.js
+++ css/index.js
@@ -22,26 +22,27 @@ function updateTime() {
     .format("h:mm:ss:SSS [<small>]A[</small>]");
 }
 updateTime();
-setInterval(updateTime, 1);
+setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  //console.log(cityTimeZone);
-  let cityName = cityTimeZone; //replace("_", " ").split("/")[1];
+  console.log(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  console.log(cityName);
   let cityTime = moment().tz(cityTimeZone);
-  //console.log(cityTime.format("dddd Do MMMM, YYYY"));
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-    <div class="city">
-      <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("dddd Do MMMM, YYYY")}</div>
-      </div>
-      <div class="time">
-        ${cityTime.format("h:mm:ss:SSS [<small>]A[</small>]")}
-      </div>
-    </div>
-  `;
+  console.log(cityTime.format("dddd Do MMMM, YYYY"));
+  let changeCity = document.querySelector("#place");
+  changeCity.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("dddd Do MMMM, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small>
+          </div>
+        </div>
+        `;
 }
 
 let citiesSelectedElement = document.querySelector("#chosen-city");
